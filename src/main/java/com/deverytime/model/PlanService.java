7457572011fe182775dcf2ea210a95b30f1aca1d
JJanE2 <aaa1685@naver.com>
2026-03-24 22:02:50 +0900

    plan list controller 까지 연결

--- src/main/java/com/deverytime/model/PlanService.java
+++ src/main/java/com/deverytime/model/PlanService.java
@@ -1,6 +1,14 @@
 package com.deverytime.model;
 
+import java.util.ArrayList;
+
 public class PlanService {
+	
+	public ArrayList<PlanDto> list (int memberSeq) {
+		PlanDao dao = new PlanDao();
+		
+		return dao.list(memberSeq);
+	}
 	
 	public PlanDto add() {
 		PlanDao dao = new PlanDao();
