a822c5a3a57a7295b8e861a781993661414ceafe
pinn.public <pinn.public@gmail.com>
2026-03-24 16:14:09 +0900

    StudyDto 생성

---
+++ src/main/java/com/test/deverytime/model/StudyDto.java
@@ -0,0 +1,12 @@
+package com.test.deverytime.model;
+
+public class StudyDto {
+
+	private String seq;
+	private String name;
+	private String description;
+	private String capacity;
+	private String status;
+	private String createDate;
+	
+}
