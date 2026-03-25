529bf7fe53e5da2cfef0ace97951058c24226ecf
wauder0430 <wlaudaoq123@naver.com>
2026-03-25 16:12:07 +0900

    init: 게시판 상세조회 초기 설정

---
+++ src/main/java/com/deverytime/board/freeboard/View.java
@@ -0,0 +1,23 @@
+package com.deverytime.board.freeboard;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/board/freeboard/view.do")
+public class View extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//View.java
+		
+
+		req.getRequestDispatcher("/WEB-INF/views/board/freeboard/view.jsp").forward(req, resp);
+	}
+
+}
