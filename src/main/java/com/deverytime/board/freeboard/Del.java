60496c54dadb23e67c17c545fee69c4f0c6b9245
wauder0430 <wlaudaoq123@naver.com>
2026-03-26 17:33:18 +0900

    init: 게시판 삭제 초기

---
+++ src/main/java/com/deverytime/board/freeboard/Del.java
@@ -0,0 +1,22 @@
+package com.deverytime.board.freeboard;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/board/freeboard/del.do")
+public class Del extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//Del.java
+		
+
+	}
+
+}
