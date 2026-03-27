4780f86902db30f22997d41a7310f9df9cfec3d9
JJanE2 <aaa1685@naver.com>
2026-03-27 15:25:54 +0900

    feat: add admin-memberList method

--- src/main/java/com/deverytime/admin/ListMember.java
+++ src/main/java/com/deverytime/admin/ListMember.java
@@ -1,5 +1,33 @@
 package com.deverytime.admin;
 
-public class ListMember {
+import java.io.IOException;
+import java.util.HashMap;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/list-member.do")
+public class ListMember extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		AdminService service = new AdminService();
+		String page = req.getParameter("page");
+		String type = req.getParameter("type");
+		String word = req.getParameter("word");
+
+		HashMap<String, Object> result = service.getMemberList(page, type, word);
+
+		req.setAttribute("list", result.get("list"));
+		req.setAttribute("pagebar", result.get("pagebar"));
+		req.setAttribute("type", result.get("type"));
+		req.setAttribute("word", result.get("word"));
+		req.setAttribute("totalCount", result.get("totalCount"));
+
+		req.getRequestDispatcher("/WEB-INF/views/admin/member-list.jsp").forward(req, resp);
+	}
 }
