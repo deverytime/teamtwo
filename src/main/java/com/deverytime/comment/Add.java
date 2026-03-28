45cd44dcde1e86abbd58d8f94a275285b6609729
wauder0430 <wlaudaoq123@naver.com>
2026-03-28 22:15:42 +0900

    feat: 게시판 댓글 등록

--- src/main/java/com/deverytime/comment/Add.java
+++ src/main/java/com/deverytime/comment/Add.java
@@ -7,16 +7,64 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.deverytime.model.BoardDto;
+import com.deverytime.model.CommentDto;
 
 @WebServlet(value = "/comment/add.do")
 public class Add extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		//Add.java
+		// 1.정보 받아오기
 		
-		req.getRequestDispatcher("/WEB-INF/views/board/freeboard/view.jsp").forward(req, resp);
+	    req.setCharacterEncoding("UTF-8");
+
+		String board = req.getParameter("board");
+		String seq = req.getParameter("seq");
+		
+		BoardDto dto = new BoardDto();
+		dto.setBoardType(board);
+		dto.setSeq(seq);
+		
+		// 검색 카테고리 추가
+		String category = req.getParameter("category");
+		String searchType = req.getParameter("searchType");
+		String keyword = req.getParameter("keyword");
+		
+		dto.setCategory(category);
+		dto.setSearchType(searchType);
+		dto.setKeyword(keyword);
+		
+		//댓글 정보 
+		HttpSession session = req.getSession();
+		CommentDto cDto = new CommentDto();
+		String postSeq = seq; // 댓글 처리를 위한 글번호
+		String id = (String)session.getAttribute("auth");
+		String content = req.getParameter("content");
+		
+		cDto.setPostSeq(postSeq);
+		cDto.setContent(content);
+		cDto.setId(id);
+		
+		CommentService service = new CommentService();
+		
+		int result = service.add(cDto);
+		
+		// 리다이렉트 (상태보존)
+		// 검색어 한글 오류 방지
+		String keywordEnc = java.net.URLEncoder.encode(keyword, "UTF-8");
+		String redirectUrl = "/teamtwo/board/freeboard/view.do?seq=" + postSeq + 
+		    "&board=" + board + 
+		    "&category=" + java.net.URLEncoder.encode(category, "UTF-8") +
+		    "&searchType=" + searchType + 
+		    "&keyword=" + keywordEnc; 
+		    //"&page=" + page;
+
+		resp.sendRedirect(redirectUrl);
 
 	}
 	
---
+++ src/main/java/com/deverytime/comment/CommentService.java
@@ -0,0 +1,23 @@
+package com.deverytime.comment;
+
+import com.deverytime.model.CommentDao;
+import com.deverytime.model.CommentDto;
+
+public class CommentService {
+
+	public int add(CommentDto cDto) {
+		
+		CommentDao dao = new CommentDao();
+
+		// DB에 인서트하기전에 id로 memberSeq를 알아오기
+		String memberSeq = dao.getMemberSeqById(cDto.getId());
+
+		cDto.setMemberSeq(memberSeq);
+
+		int result = dao.add(cDto);
+
+		return result;
+		
+	}
+
+}
